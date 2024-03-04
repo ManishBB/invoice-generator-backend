892a2ab7ddeecf0dd67dc243c33ba864e7537974
Manish <mbb7224@gmail.com>
2024-03-04 23:50:49 +0530

    Updated pdf layout

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -13,97 +13,91 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const createInvoice = async (req, res) => {
-    const { pdfData } = req.body;
+    try {
+        const { pdfData } = req.body;
 
-    const { products, total, gst, totalWithGst } = pdfData;
+        const { products, total, gst, totalWithGst } = pdfData;
 
-    const newInvoice = new Invoice({
-        products: products,
-        total: total,
-        gst: gst,
-        totalWithGst: totalWithGst,
-    });
-
-    const createdInvoice = await newInvoice.save();
-
-    // Generate PDF
-    const doc = new PDFDocument();
-    let buffers = [];
-    doc.on("data", buffers.push.bind(buffers));
-    doc.on("end", () => {
-        let pdfData = Buffer.concat(buffers);
-        res.writeHead(200, {
-            "Content-Length": Buffer.byteLength(pdfData),
-            "Content-Type": "application/pdf",
-            "Content-disposition": "attachment;filename=invoice.pdf",
-        }).end(pdfData);
-    });
-
-    // Header
-    doc.fontSize(20).text("Invoice Generator", 50, 50);
-    doc.image(`${__dirname}/trophy.png`, 500, 45, { width: 30 }); // Adjust path and dimensions as needed
-
-    // Table Headings
-    doc.moveDown();
-    doc.fontSize(14).text("Product Name", 50, 100);
-    doc.text("Quantity", 200, 100);
-    doc.text("Rate", 300, 100);
-    doc.text("Total", 400, 100);
-
-    // Separator Line
-    doc.moveTo(50, 120).lineTo(550, 120).stroke();
-
-    // Products List
-    let y = 130;
-    products.forEach((product) => {
-        doc.fontSize(12).text(product.name, 50, y);
-        doc.text(product.quantity, 200, y);
-        doc.text(product.rate, 300, y);
-        doc.text(product.rate * product.quantity, 400, y);
-        y += 20;
-    });
-
-    // Final Separator Line
-    doc.moveTo(50, y).lineTo(550, y).stroke();
-
-    // Totals
-    doc.fontSize(12).text(`Total: ${total}`, 400, y + 20);
-    doc.text(`GST (18%): ${gst}`, 400, y + 40);
-    doc.text(`Grand Total: ${totalWithGst}`, 400, y + 60);
-
-    // Footer with Terms & Conditions
-    doc.rect(
-        doc.page.margins.left,
-        doc.page.height - 50,
-        doc.page.width - doc.page.margins.right * 2,
-        50
-    ).fill("#000");
-    doc.fillColor("#FFF")
-        .fontSize(10)
-        .text("Terms and Conditions", 50, doc.page.height - 40, {
-            width: 500,
-            align: "center",
+        const newInvoice = new Invoice({
+            products: products,
+            total: total,
+            gst: gst,
+            totalWithGst: totalWithGst,
         });
 
-    doc.end();
+        const createdInvoice = await newInvoice.save();
+
+        const doc = new PDFDocument();
+        let buffers = [];
+        doc.on("data", buffers.push.bind(buffers));
+        doc.on("end", () => {
+            let pdfData = Buffer.concat(buffers);
+            res.writeHead(200, {
+                "Content-Length": Buffer.byteLength(pdfData),
+                "Content-Type": "application/pdf",
+                "Content-disposition": "attachment;filename=invoice.pdf",
+            }).end(pdfData);
+        });
+
+        doc.fontSize(20)
+            .font("Helvetica-Bold")
+            .text("Invoice Generator", 50, 50);
+        doc.image(`${__dirname}/logo.png`, 450, 45, { width: 100 }); // Adjust path and dimensions as needed
+
+        doc.moveDown();
+        doc.fontSize(14).text("Product Name", 50, 100);
+        doc.text("Qty", 200, 100);
+        doc.text("Rate", 300, 100);
+        doc.text("Total", 400, 100);
+
+        doc.moveTo(50, 120).lineTo(550, 120).stroke();
+
+        let y = 130;
+        products.forEach((product) => {
+            doc.fontSize(12).font("Helvetica").text(product.name, 50, y);
+            doc.fillColor("blue").text(product.quantity, 200, y);
+            doc.fillColor("black").text(product.rate, 300, y);
+            doc.text("INR " + product.rate * product.quantity, 400, y);
+            y += 20;
+        });
+
+        doc.moveTo(50, y).lineTo(550, y).stroke();
+
+        doc.fontSize(12)
+            .font("Helvetica-Bold")
+            .text(`Total:`, 380, y + 20);
+        doc.font("Helvetica")
+            .fontSize(12)
+            .text(`${total}`, 485, y + 20);
+        doc.fillColor("gray").text(`GST:`, 380, y + 40);
+        doc.text(`18%`, 490, y + 40);
+        doc.moveTo(380, y + 60)
+            .lineTo(550, y + 60)
+            .stroke();
+        doc.fillColor("black")
+            .font("Helvetica-Bold")
+            .text(`Grand Total:`, 380, y + 70);
+        doc.fillColor("blue").text(`${totalWithGst}`, 500, y + 70);
+        doc.moveTo(380, y + 90)
+            .lineTo(550, y + 90)
+            .stroke();
+
+        const footerImagePath = `${__dirname}/tnc.png`;
+        const imageWidth = 500;
+        const imageHeight = 70;
+        const imageX = 50;
+        const imageY =
+            doc.page.height - doc.page.margins.bottom - imageHeight + 40;
+
+        doc.image(footerImagePath, imageX, imageY, {
+            width: imageWidth,
+            height: imageHeight,
+        });
+
+        doc.end();
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export { createInvoice };
-
-// doc.fontSize(25).text("Invoice", 100, 80);
-
-// // List products
-// let y = 180;
-// products.forEach((product) => {
-//     doc.text(
-//         `Product: ${product.name}, Quantity: ${product.quantity}, Rate: ${product.rate}`,
-//         100,
-//         y
-//     );
-//     y += 20;
-// });
-
-// doc.text(`Total: ${total}`, 100, 120);
-// doc.text(`GST (18%): ${gst}`, 100, 140);
-// doc.text(`Total with GST: ${totalWithGst}`, 100, 160);
-// doc.end();
